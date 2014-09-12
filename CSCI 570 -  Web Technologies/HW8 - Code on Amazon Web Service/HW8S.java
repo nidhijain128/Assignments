1f9f79bbd0a1f63a3ebae80065e368898a55da59
nidhijain128 <nidhiprj@usc.edu>
2014-09-12 02:35:02 -0700

    --all

---
+++ CSCI 570 -  Web Technologies/HW8 - Code on Amazon Web Service/HW8S.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HW8S extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("GET Request. No Form Data Posted");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse res)
+    throws IOException, ServletException
+    {
+        Enumeration e = request.getParameterNames();
+		PrintWriter out = res.getWriter ();
+        while (e.hasMoreElements()) {
+            String name = (String)e.nextElement();
+            String value = request.getParameter(name);
+            out.println(name + " = " + value);
+        }
+    }
+}
